cb3a39543f21d4bea920edd99d3e1a1b0517ffcc
Marvin Rösch <marvinroesch99@gmail.com>
2020-10-21 18:33:45 +0200

    [deploy] Add separate dev build wihtout PWA and to reduce bundle size

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 # build stage
 FROM node:14.5-alpine as build-stage
+
+ARG PUBLIC_URL=/
+
 COPY ./ /app
 WORKDIR /app
-RUN yarn install && yarn run compileLangJsons && cp /app/build/lang/* /app/src/lang && yarn run build
+RUN yarn install && yarn run compileLangJsons && cp /app/build/lang/* /app/src/lang && VUE_APP_PUBLIC_URL=${PUBLIC_URL} yarn run build
 
 FROM nginx:stable-alpine
 RUN mkdir /app
-COPY --from=build-stage /app/dist /app
+COPY --from=build-stage /app/dist/release /app
 COPY nginx.conf /etc/nginx/nginx.conf
--- src/dev.js
+++ src/dev.js
@@ -3,6 +3,7 @@ import VueDragscroll from 'vue-dragscroll'
 import App from './App.vue'
 import store from './store'
 import { i18n } from '@/i18n'
+import Editor from '@/components/editor/Editor.vue'
 import { router } from '@/routes'
 import './registerServiceWorker'
 
@@ -10,9 +11,11 @@ Vue.config.productionTip = false
 
 Vue.use(VueDragscroll)
 
+const editor = window.location.hash.includes('editor')
+
 new Vue({
   i18n,
   store,
-  router,
-  render: h => h(App)
+  router: editor ? undefined : router,
+  render: h => h(editor ? Editor : App)
 }).$mount('#app')
