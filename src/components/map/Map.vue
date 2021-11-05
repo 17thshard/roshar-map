819dffdb239f0cacb6a01fe1f150f1830f211b35
Marvin Rösch <marvinroesch99@gmail.com>
2021-11-05 11:55:50 +0100

    Respect active location *and* event for Shadesmar transition, fixes #125

--- src/components/map/Map.vue
+++ src/components/map/Map.vue
@@ -346,7 +346,10 @@ export default {
 
       if (event !== null && event.shadesmar) {
         this.enterLayer('shadesmar')
-      } else if (oldEvent !== null && oldEvent.shadesmar && !this.layersActive.shadesmar) {
+      } else if (
+        oldEvent !== null && oldEvent.shadesmar &&
+        !this.layersActive.shadesmar && !(this.activeLocation !== null && this.activeLocation.shadesmar)
+      ) {
         this.leaveLayer('shadesmar')
       }
 
@@ -385,7 +388,10 @@ export default {
     onLocationChanged (location, oldLocation) {
       if (location !== null && location.shadesmar) {
         this.enterLayer('shadesmar')
-      } else if (oldLocation !== null && oldLocation.shadesmar && !this.layersActive.shadesmar) {
+      } else if (
+        oldLocation !== null && oldLocation.shadesmar &&
+        !this.layersActive.shadesmar && !(this.activeEvent !== null && this.activeEvent.shadesmar)
+      ) {
         this.leaveLayer('shadesmar')
       }
 
